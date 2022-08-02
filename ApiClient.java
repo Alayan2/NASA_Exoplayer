96639e2989533f219f669cc71ae3af65f80f6061
Natalie Eveliz Ayala <90277439+Alayan2@users.noreply.github.com>
2022-08-02 14:04:03 -0400

    Bug, listeners, related video query updates
    NEW:
      - Fixed bug: app crashed if collection was empty. Added conditional handler and toast notification if results are empty.
      - Added onClickListener to image on search results
      - Added an expandable textview section for description under video.
      - Used keywords of selected video as query for 'related videos' section
      
     TODO:
      - Add loading image when user first opens app
      - Figure out if black ExoPlayer screen that appears when ExoPlayer activity is loaded can be removed
      - Improve search bar functionality (autocomplete, etc using Google search dialogue)
      - Add Home button
      - Make See More link expansion retractable

---
+++ ApiClient.java
@@ -0,0 +1,21 @@
+package com.exoplayer.exoplayerapp;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+// CREATES THE RETROFIT INSTANCE
+
+public class ApiClient {
+    public static String BASE_URL ="https://images-api.nasa.gov/";
+    private static Retrofit retrofit;
+    public static Retrofit getClient(OkHttpClient okHttpClient){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
